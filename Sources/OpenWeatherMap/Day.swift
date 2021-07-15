58f7cdbf580b09089f565f81f5dd271a87a2a011
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-07-15 12:21:10 -0500

    Added `Day`

---
+++ Sources/OpenWeatherMap/Day.swift
@@ -0,0 +1,18 @@
+//
+//  Day.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 15/07/21.
+//
+
+/// OpenWeatherMap Day
+public enum Day: String, Codable, CaseIterable {
+    
+    case sunday         = "Sun"
+    case monday         = "Mon"
+    case tuesday        = "Tue"
+    case wednesday      = "Wed"
+    case thursday       = "Thu"
+    case friday         = "Fri"
+    case saturday       = "Sat"
+}
