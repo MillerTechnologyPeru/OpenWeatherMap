6934e14279683cb63015bcfa46f38950bd5383eb
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-07-15 13:48:14 -0500

    Updated unit tests

--- Tests/OpenWeatherMapTests/IconTests.swift
+++ Tests/OpenWeatherMapTests/IconTests.swift
@@ -25,4 +25,24 @@ final class IconTests: XCTestCase {
         
         XCTAssertEqual(Icon.allCases, allIcons)
     }
+    
+    func testRawRepresentable() {
+        
+        let allIcons = [
+            "01d",
+            "01n",
+            "02d",
+            "02n",
+            "03d",
+            "03n",
+            "04d",
+            "04n",
+        ]
+        
+        for (index, icon) in Icon.allCases.enumerated() {
+            let rawValue = allIcons[index]
+            XCTAssertEqual(icon.rawValue, rawValue)
+            XCTAssertEqual(Icon(rawValue: rawValue), icon)
+        }
+    }
 }
