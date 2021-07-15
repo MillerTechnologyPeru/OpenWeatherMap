8a67024349b952779a3b5d2ae4508cdee857fc66
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-07-15 12:25:37 -0500

    Added `Icon`

---
+++ Sources/OpenWeatherMap/Icon.swift
@@ -0,0 +1,82 @@
+//
+//  Icon.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 15/07/21.
+//
+
+/// OpenWeatherMap Icon Name
+public struct Icon: Equatable, Hashable {
+    
+    /// Time of day.
+    public var time: Time
+    
+    /// Weather condition.
+    public var condition: WeatherCondition
+    
+    public init(condition: WeatherCondition,
+                time: Time) {
+        
+        self.condition = condition
+        self.time = time
+    }
+}
+
+public extension Icon {
+    
+    static func day(_ condition: WeatherCondition) -> Icon {
+        return Icon(condition: condition, time: .day)
+    }
+    
+    static func night(_ condition: WeatherCondition) -> Icon {
+        return Icon(condition: condition, time: .night)
+    }
+}
+
+// MARK: - RawRepresentable
+
+extension Icon: RawRepresentable {
+    
+    public init?(rawValue: String) {
+        guard rawValue.count == 3,
+              let condition = UInt(rawValue.prefix(2)).flatMap(WeatherCondition.init),
+              let time = Time(rawValue: String(rawValue.suffix(1)))
+            else { return nil }
+        
+        self.init(condition: condition, time: time)
+    }
+    
+    public var rawValue: String {
+        return String(format: "%02d", condition.rawValue) + time.rawValue
+    }
+}
+
+// MARK: - Supporting Types
+
+public extension Icon {
+    
+    enum Time: String {
+        case day = "d"
+        case night = "n"
+    }
+}
+
+public extension Icon {
+    
+    enum WeatherCondition: UInt {
+        
+        /// Clear Sky
+        case clearSky           = 01
+        
+        /// Few Clouds
+        case fewClouds          = 02
+        
+        /// Scattered Clouds
+        case scatteredClouds    = 03
+        
+        /// Broken Clouds
+        case brokenClouds       = 04
+        
+        // TODO: Complete List
+    }
+}
